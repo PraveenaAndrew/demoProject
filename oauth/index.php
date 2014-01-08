c096308d65dde8d3fd409b6cb9bc9a3bf0bf3692
xavierdominic <dominicxavier88@yahoo.co.in>
2014-01-08 16:52:36 +0530

    my project

---
+++ oauth/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login with gmail</title>
+<style type="text/css">
+body
+{background:url('images1.jpg') repeat; }
+
+#wrapper{width:960px; margin:0 auto; margin-top:150px;}
+
+.button
+{display:block; height:40px; padding:0 20px; width:400px; background:Black ; text-decoration:none; 
+font:italic 28px Verdana, Geneva, sans-serif; color:coral; line-height:40px; text-align:center; margin-bottom:50px;}
+</style>
+</head>
+<body>
+
+<div id="wrapper">	
+<a href="https://accounts.google.com/o/oauth2/auth?client_id=217213914195.apps.googleusercontent.com&redirect_uri=http://localhost/Perl-Php/oauth/oauth/validate.php&scope=https://www.google.com/m8/feeds/&response_type=code" class="button">Login Using Google</a>
+</div>
+</body>
+</html>
